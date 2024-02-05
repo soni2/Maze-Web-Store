b04961f50e98904448c35429bef387a63c65d26c
soni2 <soni2gf@gmail.com>
2024-02-04 21:02:22 -0400

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import { AuthButton } from "@/Components/AuthButton";
+import AuthButtonServer from "@/Components/AuthButtonServer";
 import { LoginForm } from "@/Components/LoginForm";
 
 export default async function Home(props) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <LoginForm />
-      <AuthButton />
+      <AuthButtonServer />
     </main>
   );
 }
