ea10caded2f37538c70681b7b2cb75b6f9da0dae
soni2 <soni2gf@gmail.com>
2024-04-10 08:31:42 -0400

    .

--- src/app/@modal/(.)products/[id]/page.js
+++ src/app/@modal/(.)products/[id]/page.js
@@ -2,7 +2,7 @@ import { Modal } from "@/Components/Modal";
 import { ProductModal } from "./ProductModal";
 
 export default async function Product({ params }) {
-  const url = `http://localhost:3000/items?productId=${params.id}`;
+  const url = `/items?productId=${params.id}`;
 
   const res = await fetch(url);
   const data = await res.json();
